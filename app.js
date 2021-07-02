758e5379685b0d1d19b96de74882019e52522c1c
hasanmiaweb <hasanmiaweb@gmail.com>
2021-07-02 23:34:55 +0600

    some update

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-// Express 
-const express = require('express');
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const port = 3000
 
-const app = express();
+app.use(express.json())
 
-console.log(app);
+app.get('/', (req, res) => {
+  res.send('<h1 style="color:green; text-align:center">The server is running successfully</h2>')
+})
+
+
+app.get('/api/students', (req,res)=>{
+  fs.readFile('./db.json', 'utf-8', (error, data)=>{
+    const students =JSON.parse(data).students
+    console.log(students);
+    res.send(students)
+  })
+})
+
+app.post('/api/students', (req, res)=>{
+  console.log(req.body);
+  res.send("Data Send")
+})
+
+app.listen(port, () => {
+  console.log(`Server is Running On PORT: ${port}`)
+})
